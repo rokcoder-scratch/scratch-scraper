5065d6f69c298acc72515ac0f27daffa5ee50509
RokCoder <github@roksoft.co.uk>
2021-02-09 09:28:07 +0000

    Testing regex for profiledata

--- createprofile.py
+++ createprofile.py
@@ -1,6 +1,12 @@
 import extractassets
 import pack
 
+# with open('temp/project.json', 'r') as file:
+#     project = file.readline()
+# reg = re.compile('^"name:["\:"profileData"')
+# m1 = reg.search(project)
+# print(m1)
+
 # Extract all the required assets for the profile or studio we're creating the profile for
 extractassets.PullAssets()
 
